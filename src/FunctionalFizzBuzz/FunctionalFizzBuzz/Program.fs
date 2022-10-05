7c732504a8beba6ed3885f9bdb5a6462cec401c0
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-10-05 16:16:52 +0200

    Solved

--- src/FunctionalFizzBuzz/FunctionalFizzBuzz/Program.fs
+++ src/FunctionalFizzBuzz/FunctionalFizzBuzz/Program.fs
@@ -1,7 +1,52 @@
 ﻿module FunctionalFizzBuzz
 
+type FizzBuzz =
+  | Fizz
+  | Buzz
+  | FizzBuzz
+  | Number of int
+
+
+let doFizzBuzz (tfb: int -> FizzBuzz) (printer: FizzBuzz -> string) (n: int) : string =
+  let fb = tfb n
+  printer fb
+  
+let toFizzBuzz (n: int) : FizzBuzz =
+  match n with
+  | number when number % 15 = 0 -> FizzBuzz
+  | number when number % 3 = 0 -> Fizz
+  | number when number % 5 = 0 -> Buzz
+  | number -> Number number
+  
+let printer (fb: FizzBuzz) : string =
+  match fb with
+  | Fizz -> "Fizz"
+  | Buzz -> "Buzz"
+  | FizzBuzz -> "FizzBuzz"
+  | Number n -> string n
+
 let fizzBuzz : int -> string =
-  failwith "not implemented"
+  doFizzBuzz toFizzBuzz printer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 [<EntryPoint>]
