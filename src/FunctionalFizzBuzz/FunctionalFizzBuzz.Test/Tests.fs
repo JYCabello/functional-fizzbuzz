847b8c0465062f3477f6813657a6055466a3fe06
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-10-09 09:48:33 +0200

    folding

--- src/FunctionalFizzBuzz/FunctionalFizzBuzz.Test/Tests.fs
+++ src/FunctionalFizzBuzz/FunctionalFizzBuzz.Test/Tests.fs
@@ -15,4 +15,18 @@ open FunctionalFizzBuzz
 [<InlineData(45, "FizzBuzz")>]
 let ``My test`` input output =
     Assert.Equal(output, fizzBuzz input)
+
+[<Theory>]
+[<InlineData(1, "1")>]
+[<InlineData(7, "Bar")>]
+[<InlineData(3, "Fizz")>]
+[<InlineData(9, "Fizz")>]
+[<InlineData(5, "Buzz")>]
+[<InlineData(10, "Buzz")>]
+[<InlineData(15, "FizzBuzz")>]
+[<InlineData(45, "FizzBuzz")>]
+[<InlineData(35, "BuzzBar")>]
+[<InlineData(21, "FizzBar")>]
+[<InlineData(105, "FizzBuzzBar")>]
+let ``With Bar`` input output =
     Assert.Equal(output, FunctionalFizzBuzzFolding.fizzBuzz input)
--- src/FunctionalFizzBuzz/FunctionalFizzBuzz/Folding.fs
+++ src/FunctionalFizzBuzz/FunctionalFizzBuzz/Folding.fs
@@ -3,15 +3,20 @@ open FunctionalFizzBuzzCommon
 
 let legend =
   [ (3, "Fizz")
-    (5, "Buzz") ]
-let fizzBuzz : int -> string =
-  fun n ->
-    let folder acc (dividend, output) =
-      match n with
-      | IsMultipleOf dividend -> sprintf "%s%s" acc output
-      | _ -> acc
-    legend
-    |> List.fold folder ""
-    |> function
-      | "" -> string n
-      | r -> r
+    (5, "Buzz")
+    (7, "Bar") ]
+
+let fizzBuzz n =
+  let folder acc (dividend, output) =
+    match n with
+    | IsMultipleOf dividend -> sprintf "%s%s" acc output
+    | _ -> acc
+
+  let defaulter =
+    function
+    | "" -> string n
+    | r -> r
+
+  ("", legend)
+  ||> List.fold folder 
+  |> defaulter
